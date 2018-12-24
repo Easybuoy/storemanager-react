0fdc8e22b73e918e312d348745e150b02d12c1f9
Ezekiel <ekunolaeasybuoy@gmail.com>
2018-12-24 22:07:12 +0100

    [ft #162780548] created login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import './App.css';
+
+import Login from './components/Login/Login';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome To Store Manager react</h1>
-            </div>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={ Login } />
+                    <Route path='*' component={() => 'Not found'} />
+                </Switch>
+                
+            </Router>
+            
         );
     }
 }
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { Redirect } from 'react-router-dom';
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            isLoggedIn: 0,
+            redirect: false,
+        }
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+
+    onChange(event){
+        this.setState({[event.target.name] : event.target.value});
+    }
+
+    setToken(token){
+        localStorage.setItem('token', token);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        let status = 0;
+        fetch('https://store--manager.herokuapp.com/api/v1/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({email: this.state.email, password: this.state.password})
+        })
+        .then((res) => {
+            status = res.status;
+            return res.json()
+        })
+        .then(data => {
+            const response = data.data;
+            const token = data.data.token;
+            switch (status) {
+                case 200:
+                this.setToken(token);
+                this.setState( {redirect: true})
+                    break;
+                case 400:
+                  if (response.email && response.password) {
+                    return alert('Email & Password fields are required');
+                  }
+                  alert(response.email || response.password);
+                  break;
+                case 404:
+                  alert(response.email);
+                  break;
+                case 401:
+                   alert(response.password);
+                    break;
+                default:
+                  return alert('Error loggin in');
+                }
+        })
+        .catch(err => alert('Error Loggin In...Please Try Again'));
+    }
+
+    render() {
+        if (this.state.redirect) {
+        return (
+            <Redirect to="/dashboard" />
+        );
+        }
+
+        return (
+            <div id="loginbody">
+        <div className="loginbox">
+            <img src="https://store--manager.herokuapp.com/assets/uploads/users/default-avatar.png" className="avatar"/>
+            <h2 className="text-center">Login to access store manager</h2>
+            <form method="GET" action="./dashboard.html">
+                <p>Username</p>
+                <input onChange={this.onChange} type="text" id="loginusername" name="email" placeholder="Enter Email" />
+                <p>Password</p>
+                <input onChange= {this.onChange} type="password" id="loginpassword" name="password" placeholder="Enter Password" />
+                <input onClick={this.onSubmit} type="submit" name="submit" id="loginsubmit" value="Login" />
+            </form>
+        </div>
+
+        <footer id="footer" className="footerstickbottom">
+            <p>Store Manager Copyright &copy; 2018</p>
+        </footer>
+            </div>
+        );
+    }
+}
+
+export default Login;
