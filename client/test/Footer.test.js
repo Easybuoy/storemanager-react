130e81e5a8d0a4f6a5ee497b0c8e9f1aab5c1aad
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-02-17 20:51:55 +0100

    chore(test)Loading and Footer component tests

---
+++ client/test/Footer.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Footer from '../src/components/Common/Footer';
+
+describe('<Footer />', () => {
+    it('should have 1 div', () => {
+        const wrapper = shallow(<Footer />);
+        expect(wrapper.find('div').length).toEqual(1);
+    });
+
+    it('should have 1 footer', () => {
+        const wrapper = shallow(<Footer />);
+        expect(wrapper.find('footer').length).toEqual(1);
+    });
+
+    it('renders the footer component correctly', () => {
+        const tree = renderer.create(<Footer />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+})
