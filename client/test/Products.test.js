7a01e1272e127cb3e859ecba025ab8e3b915e2dd
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-02-18 12:49:07 +0100

    chore(test) Write tests

---
+++ client/test/Products.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Products from '../src/components/Products/Products';
+
+describe('<Products />', () => {
+    it('renders the Products component correctly', () => {
+        const wrapper = shallow(<Products />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
