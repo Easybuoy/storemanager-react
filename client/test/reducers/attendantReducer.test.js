ac6d9a4590854e9fd4b8b5917b6a9abd9118672a
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-02-22 11:03:31 +0100

    chore(test) Write action and reducers tests

--- client/test/reducers/attendantReducer.test.js
+++ client/test/reducers/attendantReducer.test.js
@@ -1,5 +1,5 @@
 import attendantReducer from '../../src/reducers/attendantReducer';
-import { SET_ATTENDANT_CREATED } from '../../src/actions/types';
+import { SET_ATTENDANT_CREATED, RESET_ATTENDANT_CREATED, SET_ATTENDANT_LOADING, SET_ATTENDANTS } from '../../src/actions/types';
 
 describe('attendantReducer', () => {
     it('should return default state', () => {
@@ -18,7 +18,31 @@ describe('attendantReducer', () => {
             payload: { id: 1, name: 'Ekunola Ezekiel', status: 1}
         }
         const state = attendantReducer(undefined, action);
-        console.log(state)
         expect(state).toEqual({loading: false, isAttendantCreated: action.payload, attendants: []});
+    });
+
+    it('should reset attendant created', () => {
+        const action = {
+            type: RESET_ATTENDANT_CREATED,
+        }
+        const state = attendantReducer(undefined, action);
+        expect(state).toEqual({loading: false, isAttendantCreated: false, attendants: []});
+    });
+
+    it('should set attendant loading', () => {
+        const action = {
+            type: SET_ATTENDANT_LOADING,
+        }
+        const state = attendantReducer(undefined, action);
+        expect(state).toEqual({loading: true, isAttendantCreated: false, attendants: []});
+    });
+
+    it('should set attendants', () => {
+        const action = {
+            type: SET_ATTENDANTS,
+            payload: [{ id: 1, name: 'Ekunola Ezekiel', status: 1}]
+        }
+        const state = attendantReducer(undefined, action);
+        expect(state).toEqual({loading: false, isAttendantCreated: false, attendants: action.payload});
     });
 });
