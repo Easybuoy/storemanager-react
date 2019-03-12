26d83bf1ef856b0f763e852adf83bae6cbc2990b
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-03-12 02:02:39 +0100

    (bug) Fix webpack error on heroku - Create a webpack.common.js - Create a webpack.dev.js file - Create a webpack.prod.js file [Fixes #164559270]

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,13 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'false',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
+});
